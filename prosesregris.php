e99e7abbafb5b581fea8cceb6e8dfba0d5396348
ekobahran <43519911+ekobahran@users.noreply.github.com>
2018-10-02 16:58:53 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,23 @@
-<?php
 
+<?php
+ $hostname   = "localhost";
+ $username = "root";
+ $pass     = "";
+ $db       = "pendaftaran mahasiswa";
+ $con     = new mysqli($hostname, $username, $pass, $db);
+ if($con==false){
+  die("koneksi gagal". $con->connect_error);
+ }
+  if (isset($_POST['login'])) {
+   $nim=$_POST['nim'];
+   $nama=$_POST['nama'];
+   $email=($_POST['email']);
+ 
+ $sql="INSERT INTO pendaftaran(nim,nama,email) VALUES ('$nim','$nama','$email')";
+   if(mysqli_query($con,$sql)){
+    echo "Database sudah masuk";
+   }else{
+    echo "error";;
+   }
 
 ?>
